5d5508b1b4f191ae22de815eccd3a5ac5e845cea
ToperPlayYT <53517888+ToperPlayYT@users.noreply.github.com>
2021-07-06 18:29:32 +0300

    Add files via upload

--- bypass.js
+++ bypass.js
@@ -1,2 +1,4 @@
+if (document.getElementsByClassName("form-control")[0].placeholder != "Введите ваш URL") {
 alert("LeitUP Bypassed");
 window.location.href = document.getElementsByClassName("form-control")[0].placeholder;
+}
